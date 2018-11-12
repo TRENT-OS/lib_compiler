a0634e6fa0630487024a084b87fc8e4e5c3d72d4
Carmelo Pintaudi <carmelo.pintaudi@hensoldt-cyber.com>
2018-11-12 17:30:15 +0100

    Remove dependency on config fake lib. Make the config files possible to be passed by cmake variables

---
+++ include/lib_compiler/compiler.h
@@ -0,0 +1,29 @@
+# pragma once
+
+#ifndef INLINE
+#  if __GNUC__ && !__GNUC_STDC_INLINE__
+#    if __OPTIMIZE__
+#      define INLINE extern inline
+#    else
+#      define INLINE static inline
+#    endif
+# else
+#  define INLINE inline
+# endif
+#endif
+
+#if !defined DECL_UNUSED_VAR
+#   if defined(__GNUC__)
+#       define DECL_UNUSED_VAR(x) x __attribute__((unused))
+#   elif defined(__LCLINT__)
+#       define DECL_UNUSED_VAR(x) /*@unused@*/ x
+#   elif defined(__cplusplus)
+#       define DECL_UNUSED_VAR(x)
+#   else
+#       define DECL_UNUSED_VAR(x) x
+#   endif
+#endif
+
+#if !defined(UNUSED_VAR)
+#   define UNUSED_VAR(x) (void) x
+#endif
