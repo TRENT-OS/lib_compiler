0368fef39d4656c8d88a2ee043ac5d0fc6658d49
Carmelo Pintaudi <c.pintaudi@gmail.com>
2019-03-21 14:07:34 +0100

    [FIX] SEOS-90: fix INLINE definition

--- include/lib_compiler/compiler.h
+++ include/lib_compiler/compiler.h
@@ -1,15 +1,19 @@
 # pragma once
 
 #ifndef INLINE
-#  if __GNUC__ && !__GNUC_STDC_INLINE__
-#    if __OPTIMIZE__
-#      define INLINE extern inline
+#  if __GNUC__
+#    if !__GNUC_STDC_INLINE__
+#      if __OPTIMIZE__
+#        define INLINE extern inline
+#      else
+#        define INLINE static inline
+#      endif
 #    else
 #      define INLINE static inline
 #    endif
-# else
-#  define INLINE inline
-# endif
+#  else
+#    define INLINE inline
+#  endif
 #endif
 
 #if !defined DECL_UNUSED_VAR
